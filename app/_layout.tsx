8649ef3b856394062d446f8d2f8a0be069a7d580
vsmaliakou <vsmaliakou@yandex.by>
2024-06-24 14:28:42 +0300

    feat: Expo Router Authentication & Tab Bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,11 @@ import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
-import { Link, Stack, useRouter } from "expo-router";
+import { Link, Stack, useRouter, useSegments } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import { TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 import * as SecureStore from "expo-secure-store";
@@ -45,7 +45,8 @@ const InitialLayout = () => {
     ...FontAwesome.font,
   });
   const router = useRouter();
-  const { isSignedIn } = useAuth();
+  const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -59,11 +60,21 @@ const InitialLayout = () => {
   }, [loaded]);
 
   useEffect(() => {
-    console.log("isSignedIn", isSignedIn);
+    if (!isLoaded) {
+      return;
+    }
+
+    const inAuthGroup = segments[0] === "(authenticated)";
+
+    if (isSignedIn && !inAuthGroup) {
+      router.replace("/(authenticated)/(tabs)/home");
+    } else if (!isSignedIn) {
+      router.replace("/");
+    }
   }, [isSignedIn]);
 
-  if (!loaded) {
-    return null;
+  if (!loaded || !isLoaded) {
+    return <Text>Loading...</Text>;
   }
 
   return (
@@ -126,6 +137,10 @@ const InitialLayout = () => {
               </TouchableOpacity>
             ),
           }}
+        />
+        <Stack.Screen
+          name="(authenticated)/(tabs)"
+          options={{ headerShown: false }}
         />
       </Stack>
 
