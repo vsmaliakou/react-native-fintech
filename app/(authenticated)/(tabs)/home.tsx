6f3547a7c71d683919816eab0ebd9aaab9e8a73f
vsmaliakou <vsmaliakou@yandex.by>
2024-06-24 16:41:57 +0300

    feat: Home Screen with Native Dropdown

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,12 +1,58 @@
+import Dropdown from "@/components/Dropdown";
+import RoundBtn from "@/components/RoundBtn";
+import Colors from "@/constants/Colors";
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 const Page = () => {
+  const balance = 1420;
+
+  const onAddMoney = () => {};
+
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <ScrollView style={{ backgroundColor: Colors.background }}>
+      <View style={styles.account}>
+        <View style={styles.row}>
+          <Text style={styles.balance}>{balance}</Text>
+
+          <Text style={styles.currency}>€</Text>
+        </View>
+      </View>
+
+      <View style={styles.actionRow}>
+        <RoundBtn text="Add money" icon="add" onPress={onAddMoney} />
+        <RoundBtn text="Exchange" icon="refresh" />
+        <RoundBtn text="Details" icon="list" />
+        <Dropdown />
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  account: {
+    margin: 80,
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "center",
+    gap: 10,
+  },
+  balance: {
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  currency: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  actionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+});
 
 export default Page;
---
+++ components/RoundBtn.tsx
@@ -0,0 +1,44 @@
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+type RoundBtnProps = {
+  text: string;
+  icon: typeof Ionicons.defaultProps;
+  onPress?: () => void;
+};
+
+const RoundBtn = ({ text, icon, onPress }: RoundBtnProps) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <View style={styles.circle}>
+        <Ionicons name={icon} size={30} color={Colors.dark} />
+      </View>
+
+      <Text style={styles.label}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    gap: 10,
+  },
+  circle: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: Colors.lightGray,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: Colors.dark,
+  },
+});
+
+export default RoundBtn;
