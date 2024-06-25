8a7e9da9eb7276f7a8f423007579bc5f3d3cb065
vsmaliakou <vsmaliakou@yandex.by>
2024-06-25 18:18:28 +0300

    feat: Drag and Drop Widgets with Gestures

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,5 +1,6 @@
 import Dropdown from "@/components/Dropdown";
 import RoundBtn from "@/components/RoundBtn";
+import WidgetList from "@/components/SortableList/WidgetList";
 import Colors from "@/constants/Colors";
 import { defaultStyles } from "@/constants/Styles";
 import { useBalanceStore } from "@/store/balanceStore";
@@ -70,6 +71,10 @@ const Page = () => {
           </View>
         ))}
       </View>
+
+      <Text style={defaultStyles.sectionHeader}>Widgets</Text>
+
+      <WidgetList />
     </ScrollView>
   );
 };
